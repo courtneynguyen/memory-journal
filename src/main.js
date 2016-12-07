dbb4d0de0ff34c422e8fbc7988bfb972796dc83a
Courtney Nguyen <courtneythuy@gmail.com>
2016-12-07 00:14:22 -0700

    stuff

--- src/main.js
+++ src/main.js
@@ -1,118 +1,128 @@
-var memoryJournal = (function($){
-	var sortedPhotos = [];
-	var photos = {};
-	var timeline = {};
-	var options = {
-		shape: 'circle',
-		distance: 100
-	};
-
-	var setup = function(data){
-		photos = data;
-		sortedPhotos = photos.sort(function(a, b){
-			return a.taken > b.taken;
-		});
-		createTimeline();
-		postPhotos(sortedPhotos);
-	};
-
-	var createTimeline = function(){
-		sortedPhotos.forEach(function(photo){
-			var selectedPhoto = {id: photo._id};
-			var taken = new Date(photo.taken);
-			var month = taken.getMonth();
-			var day = taken.getDate();
-			if(!timeline[month]){
-				timeline[month] = {};
-				timeline[month][day] = {
-					pictures: []
-				};
-			}
-			if(!timeline[month][day]){
-				timeline[month][day] = {
-					pictures: []
-				}
-			}
-			timeline[month][day].pictures.push(selectedPhoto);
-		});
-		createTimelineButtons();
-	};
-
-	var createTimelineButtons = function(){
-		var months = {
-			'0': 'Jan.',
-			'1': 'Feb.',
-			'2': 'Mar.',
-			'3': 'Apr.',
-			'4': 'May',
-			'5': 'Jun.',
-			'6': 'Jul.',
-			'7': 'Aug.',
-			'8': 'Sep.',
-			'9': 'Oct.',
-			'10': 'Nov.',
-			'11': 'Dec.',
+$(document).ready(function(){
+	var memoryJournal = (function($){
+		var sortedPhotos = [];
+		var photos = {};
+		var timeline = {};
+		var options = {
+			shape: 'circle',
+			distance: 100
 		};
-		Object.keys(months).forEach(function(month){
-			if(timeline[month]){
-				var ticks = createTicks(month);
-				$('#month-time').append('<li>' + months[month] + ticks + '</li>');
-			}
-		});
 
-		addListeners();
-	};
-
-	var createTicks = function(month){
-		var ticks = '<ol>';
-		// timeline[month].ticks = Object.keys(timeline[month]).length;
-		Object.keys(timeline[month]).forEach(function(day){
-			timeline[month][day].pictures.forEach(function(photo){
-				ticks += '<li class="tick" data-photo-id="'+photo.id+'"></li>';
+		var setup = function(data){
+			photos = data;
+			sortedPhotos = photos.sort(function(a, b){
+				return a.taken > b.taken;
 			});
-			
-		});
-		// for(var x = 0; x < timeline[month].ticks; x++){
-		// 	ticks += '<li class="tick"></li>';
-		// }
-		ticks += '</ol>';
-		return ticks;
-	};
+			createTimeline();
+			postPhotos(sortedPhotos);
+			var memories = $('#memories');
+			setTimeout(function(){
+				$('html, body').animate({
+					scrollTop: memories.offset().top
+				}, 1000);
+			}, 1000);
 
-	var postPhotos = function(data){
-		
-		data.forEach(function(photograph, index){
-			if(index == 0){
-				$('.photos').append('<div id="'+photograph._id+'" class="photograph selected"><img src="data:image/jpg;base64,'+photograph.picture+'" />'+ photograph.caption +'</div>');
+			// window.scrollTo(0, memories.offset().top);
+		};
+
+		var createTimeline = function(){
+			sortedPhotos.forEach(function(photo){
+				var selectedPhoto = {id: photo._id};
+				var taken = new Date(photo.taken);
+				var month = taken.getMonth();
+				var day = taken.getDate();
+				if(!timeline[month]){
+					timeline[month] = {};
+					timeline[month][day] = {
+						pictures: []
+					};
+				}
+				if(!timeline[month][day]){
+					timeline[month][day] = {
+						pictures: []
+					}
+				}
+				timeline[month][day].pictures.push(selectedPhoto);
+			});
+			createTimelineButtons();
+		};
+
+		var createTimelineButtons = function(){
+			var months = {
+				'0': 'Jan.',
+				'1': 'Feb.',
+				'2': 'Mar.',
+				'3': 'Apr.',
+				'4': 'May',
+				'5': 'Jun.',
+				'6': 'Jul.',
+				'7': 'Aug.',
+				'8': 'Sep.',
+				'9': 'Oct.',
+				'10': 'Nov.',
+				'11': 'Dec.',
+			};
+			Object.keys(months).forEach(function(month){
+				if(timeline[month]){
+					var ticks = createTicks(month);
+					$('#month-time').append('<li>' + months[month] + ticks + '</li>');
+				}
+			});
+
+			addListeners();
+		};
+
+		var createTicks = function(month){
+			var ticks = '<ol>';
+			// timeline[month].ticks = Object.keys(timeline[month]).length;
+			Object.keys(timeline[month]).forEach(function(day){
+				timeline[month][day].pictures.forEach(function(photo){
+					ticks += '<li class="tick" data-photo-id="'+photo.id+'"></li>';
+				});
+
+			});
+			// for(var x = 0; x < timeline[month].ticks; x++){
+			// 	ticks += '<li class="tick"></li>';
+			// }
+			ticks += '</ol>';
+			return ticks;
+		};
+
+		var postPhotos = function(data){
+
+			data.forEach(function(photograph, index){
+				if(index == 0){
+					$('.photos').append('<div id="'+photograph._id+'" class="photograph selected"><img src="data:image/jpg;base64,'+photograph.picture+'" />'+ photograph.caption +'</div>');
+				}
+				else {
+					$('.photos').append('<div id="'+photograph._id+'" class="photograph"><img src="data:image/jpg;base64,'+photograph.picture+'" />'+ photograph.caption +'</div>');
+				}
+			});
+
+		};
+
+		var addListeners = function(){
+			var $dayList = $('#month-time > li > ol > li');
+			$dayList.on('click', null, $dayList, changePhoto);
+		};
+
+		var changePhoto = function(event){
+			var photoId = event.target.dataset.photoId;
+			$('.photograph').removeClass('selected');
+			$('#'+photoId).addClass('selected');
+
+			console.log(arguments);
+		};
+
+		$.ajax({
+			url: 'http://localhost:3000/photographs',
+			success: setup,
+			crossDomain: true,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
 			}
-			else {
-				$('.photos').append('<div id="'+photograph._id+'" class="photograph"><img src="data:image/jpg;base64,'+photograph.picture+'" />'+ photograph.caption +'</div>');
-			}
 		});
-		
-	};
 
-	var addListeners = function(){
-		var $dayList = $('#month-time > li > ol > li');
-		$dayList.on('click', null, $dayList, changePhoto);
-	};
-
-	var changePhoto = function(event){
-		var photoId = event.target.dataset.photoId;
-		$('.photograph').removeClass('selected');
-		$('#'+photoId).addClass('selected');
-
-		console.log(arguments);
-	};
-
-	$.ajax({
-		url: 'http://localhost:3000/photographs',
-		success: setup,
-		crossDomain: true,
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
-		}
-	});
-
-})(jQuery);
+	})(jQuery);
+});
--- upload.js
+++ upload.js
@@ -1,64 +1,105 @@
 'use strict';
 
 var fs = require('fs');
+var async = require('async');
 const exec = require('child_process').exec;
+var Photograph = require('./server/models/photograph');
+
+var createPicture = function(filename) {
+    return function (callback) {
+        var caption = 'No caption set';
+        var tags;
+        var taken;
+        var picture;
+        async.series([
+            callback => {
+                fs.readFile(`C:/Users/courtyen/Projects/memory-journal/pictures/${filename}`, 'base64', (err, data) => {
+                    picture = data;
+                    callback();
+                });
+            },
+            callback => {
+                exec(`exiv2.exe -K Exif.Image.ImageDescription -PEv C:/Users/courtyen/Projects/memory-journal/pictures/${filename}`, (error, title, stderr) => {
+                    caption = title;
+                    if (error) {
+                        console.error(error);
+                    }
+                    callback();
+                });
+            },
+            callback => {
+                exec(`exiv2.exe -K Exif.Image.XPKeywords -PEt C:/Users/courtyen/Projects/memory-journal/pictures/${filename}`, (error, pTags, stderr) => {
+                    tags = pTags.split(';');
+                    if (error) {
+                        console.error(error);
+                    }
+                    callback();
+                });
+            },
+            callback => {
+                exec(`exiv2.exe -K Exif.Photo.DateTimeOriginal -PEt C:/Users/courtyen/Projects/memory-journal/pictures/${filename}`, (error, time, stderr) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                    var splitTime = time.split(' ');
+                    try {
+                        if (splitTime.length > 1) {
+                            var monthDays = splitTime[0];
+                            var hourMinutes = splitTime[1].substr(0, 8);
+                            var correctMonthDayFormat = monthDays.replace(/:/g, '/');
+
+                            taken = new Date(correctMonthDayFormat + '-' + hourMinutes);
+                        }
+                    }
+                    catch (e) {
+                        taken = new Date();
+                    }
+                    callback();
+                });
+            },
+            callback => {
+                Photograph.create(
+                {
+                    caption: caption,
+                    tags: tags,
+                    taken: taken,
+                    picture: picture
+                }, callback);
+            }
+        ], err => {
+            callback && callback();
+        });
+    }
+};
 
 module.exports = (function() {
     var getPictureData = function(cb) {
         var pictures = [];
-        fs.readdir(`C:/Users/courtyen/Projects/memory-journal/pictures`, function (err, files) {
-            if (err) {
-                console.log(err);
-            }
-            if (files && files.length) {
-
-                try{
-                    files.forEach((filename, index) => {
-                        var caption = '';
-                        var tags = [];
-                        var taken = null;
-                        
-                        fs.readFile(`C:/Users/courtyen/Projects/memory-journal/pictures/${filename}`, 'base64', (err, data) => {
-                            exec(`exiv2.exe -K Exif.Image.ImageDescription -PEv C:/Users/courtyen/Projects/memory-journal/pictures/${filename}`, (error, title, stderr) => {
-                                caption = title;
-                                exec(`exiv2.exe -K Exif.Image.XPKeywords -PEt C:/Users/courtyen/Projects/memory-journal/pictures/${filename}`, (error, pTags, stderr) => {
-                                    tags =  pTags.split(';');
-                                    exec(`exiv2.exe -K Exif.Photo.DateTimeOriginal -PEt C:/Users/courtyen/Projects/memory-journal/pictures/${filename}`, (error, time, stderr) => {
-                                        var splitTime = time.split(' ');
-                                        if(splitTime.length > 1){
-                                            var monthDays = splitTime[0];
-                                            var hourMinutes = splitTime[1].substr(0,8);
-                                            var correctMonthDayFormat = monthDays.replace(/:/g, '/');
-
-                                            taken = new Date(correctMonthDayFormat + '-' + hourMinutes);
-                                        }
-                                        else {
-                                            taken = new Date();
-                                        }
-                                       
-                                        pictures.push({
-                                            caption: caption,
-                                            tags: tags,
-                                            taken: taken,
-                                            picture: data
-                                        });
-                                        if(index >= files.length-1){
-                                            console.log('CALLING BACK ' , pictures.length)
-                                            cb(null, pictures);
-                                        }
-                                    });
-                                });
-                            });
-                        }); 
+        var files;
+        async.series([
+            callback => {
+                fs.readdir(`C:/Users/courtyen/Projects/memory-journal/pictures`, function (err, _files) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    files = _files;
+                    callback();
+                });
+            },
+            callback => {
+                if (files && files.length) {
+                    var taskList = [];
+                    files.forEach((filename) => {
+                        taskList.push(createPicture(filename));
                     });
+                    async.series(taskList, callback);
                 }
-                catch(e){
-                    console.log('ERROR', e);
-                    cb(e, pictures);
+                else {
+                    callback();
                 }
             }
-        });
-    }
+            ]);
+    };
     return {
         getPictureData: getPictureData
     }
